6922022c0313dab8f45033c735ed1a2be45d03ac
vikasairsharma7071 <57059631+vikasairsharma7071@users.noreply.github.com>
2023-01-12 07:47:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+let expression = "";
+
+let ansBox = document.getElementById("input");
+
+function solve(e){
+    
+
+    if(e === 'answer'){
+
+        let res = eval(expression);
+        console.log(res);
+        ansBox.value = res;
+
+    }else if(e === 'clear'){
+
+        expression = "";
+        ansBox.value = "";
+
+    }else{
+        expression += e;
+        console.log(e);
+        // console.log(expression);
+        ansBox.value = expression;
+    }
+
+}
